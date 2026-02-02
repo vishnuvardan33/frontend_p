aa72a64a6e4bcc442e9dd720d88f5c1b22b1da94
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-02-02 21:28:27 +0530

    adding

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,16 @@
-//js part 6 question 1
-//let a = [1,2,3,4,5,6,7,8,9,10];
-//let num = 5;
 
-//function getarray(a,num){
-//    for(let i=0; i<a.length; i++){
-//        if(a[i]>=num){
-//            console.log(a[i]);
-//        }
-//    }
-//}
-//getarray(a,num);
+// js part 6 question 1
+let a = [1,2,3,4,5,6,7,8,9,10];
+let num = 5;
+
+function getarray(a,num){
+    for(let i=0; i<a.length; i++){
+        if(a[i]>=num){
+            console.log(a[i]);
+        }
+    }
+}
+getarray(a,num);
 
 
 //js part 6 question 2
