1cfe1a65d3bd7f89c8bf2cfc1e927c97b0e9e154
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-02-26 20:02:40 +0530

    add

--- JS/part 9/sourceApp.js
+++ JS/part 9/sourceApp.js
@@ -1,9 +1,1 @@
-let smallImages = document.getElementsByClassName("images");
-for (let i = 0; i < smallImages.length; i++) {
-    
-    let oldImg = smallImages[i].getElementsByClassName("oldImg");
-    oldImg[i].src = "spiderman.png";
-   
-    console.log(`value of image no. ${i} is changed to spiderman`);
-}
-        
+console.dir(document.querySelector(".p"));
