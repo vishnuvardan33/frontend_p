050e00f2c646dc1fac7441385c463703db7214a9
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-03-01 23:08:59 +0530

    add

--- JS/part 9/practiceQ.js
+++ JS/part 9/practiceQ.js
@@ -12,3 +12,19 @@ newh3.innerText = "i am blue";
 newh3.style.color = "blue";
 body.appendChild(newh3);
 
+document.createElement("div");
+let newdiv = document.createElement("div");
+newdiv.style.backgroundColor = "pink";
+newdiv.style.borderColor = "black";
+
+body.appendChild(newdiv);
+
+document.createElement("h1");
+let newh1 = document.createElement("h1");
+newh1.innerText = "i am in div";
+newdiv.appendChild(newh1);
+
+document.createElement("p");
+let pp2 = document.createElement("p");
+pp2.innerText =" me too";
+newdiv.appendChild(pp2)
