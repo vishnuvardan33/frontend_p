50fb7d17c92e182ba61ced8a73d0aedf7fe0672e
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-02 08:23:56 +0530

    add

--- JS/part 12/Async-func.js
+++ JS/part 12/Async-func.js
@@ -7,10 +7,13 @@ async function greet(){
     //throw "weak connection"
     return "Hello World";
 }
+
+
 greet()
 .then((result)=>{
     console.log("promise was resolved with: "+result);
 })
+
 .catch((error)=>{
     console.log("promise was rejected with: "+error);
 })
